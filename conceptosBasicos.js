3f8ed71503223b8d3e7bffbb3fafb1896a0e6282
withlovejulieth <jfuentes.plan@protonmail.com>
2024-04-03 21:24:27 -0500

    Actualización de notas de clase de la profe Luisa

---
+++ conceptosBasicos.js
@@ -0,0 +1,35 @@
+// CONSOLE.LOG -> la forma de enviar mensajes a la consola del navegador
+console.log("HOLA, DESARROLLADORES  Y PSICÓLOGOS");
+console.log("mi mensaje");
+
+
+
+// NOTAS PROFE: CONCEPTOS BÁSICO EN JAVASCRIPT
+//1. tipos de variables
+//2. Manejo del DOM
+//3. Condicionales
+//4. Ciclos
+// LAS VARIABLES EN JS -> piensen en las variables como contenedores de información
+
+// Hay 3 tipos de variables en js
+// VAR -> Un tipo de variable que ya no se usa (NO LA VAMOS A UTILIZAR) -> Cambia en el tiempo
+// LET -> Es una variable que puede cambiar en el tiempo
+// CONST -> en una variable que no puede cambiar en el tiempo
+
+//TIPOS DE DATOS O INFORMACIÓN PODEMOS ALMACENAR
+//el = en JS no es comparativo, sino que me sirve para asignar valores
+
+// Numéricos
+let numero = 7;
+
+// cadena de caracteres -> texto -> string (en programación)
+// El texto SIEMPRE debe estar dentro de comillas 
+let texto = "hola, mundo"
+
+//Booleanos -> un dato que puede ser o verdadero o falso 
+let esVerdadero = true; //true -> verdad
+let esFalso = false; // false -> falso
+
+//Arreglos -> arrays(en programación) -> me permiten almacenar varios datos al mismo tiempo en una misma variable
+let arregloNombres = ["Luisa", "Emmanuel", "Paula", "Lucila"];
+let arregloNumeros = [1, 3, 5, 8, 20];
