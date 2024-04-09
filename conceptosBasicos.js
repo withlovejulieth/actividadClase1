2ce7517f3bcbf9d6cb06ad24bd4f59e30c23f062
withlovejulieth <jfuentes.plan@protonmail.com>
2024-04-08 20:34:59 -0500

    Actualización de notas de clase del día  8 de abril de 2024

--- conceptosBasicos.js
+++ conceptosBasicos.js
@@ -2,16 +2,15 @@ Console.log("Hola, mundo");
 Console.log("Hola, desarrolladores y psicólogos");
 console.log("Este es mi mensaje");
 
-
 // Conceptos esenciales de JAVASCRIPT: 
-//1. Tipos de variables
-//2. Manejo del DOM
-//3. Condicionales
-//4. Ciclos
+//Tipos de variables
+//1. Manejo del DOM
+//2. Condicionales
+//3. Ciclos
 // LAS VARIABLES EN JS -> Contenedores de información
 
 // Hay 3 tipos de variables en JS
-// VAR -> No se emplea en la actualidad -> También cambia en el tiempom
+// VAR -> No se emplea en la actualidad, está en desuso -> También cambia en el tiempo
 // LET -> Variable que puede cambiar en el tiempo
 // CONST -> Variable que no puede cambiar en el tiempo
 
@@ -29,6 +28,27 @@ let texto = "hola, mundo"
 let esVerdadero = true; 
 let esFalso = false; 
 
-// Arreglos (arrays en programación) -> Facilitan el almacenamiento de varios datos al mismo tiempo dentro una misma variable EJ:
+// Arreglos (arrays en programación) -> Facilitan el almacenamiento de varios datos al mismo tiempo dentro una misma variable. Por lo tanto debemos recordar que deben pertener a una misma clase o tipo. EJ:
 let arregloNombres = ["Luisa", "Emmanuel", "Paula", "Lucila"];
 let arregloNumeros = [1, 3, 5, 8, 20];
+
+// Objetos - Representación de elementos de la vida real pero en el propio código. Cuenta con atributos o características y métodos o acciones que pueden llevar a cabo
+// Llaves {} para declarar objetos; corchetes [] para contener arreglos y paréntesis () para ejecutar acciones o invocar funciones 
+
+const celular = {
+    color: "blanco",
+    modelo: "Galaxy A 7",
+    ancho: 350,
+    largo: 800,
+    marca: "Samsung",
+    enviarMensajes: function(){
+        //Cuando hay llaves al lado de paréntesis se establece la acción a realizar
+        console.log("Mensaje enviado");
+    },
+
+    sumar: function(){
+        console.log(5+4);
+    }
+};
+
+// Las funciones son las acciones que realiza el objeto; pueden o no recibir datos de entrada, pero es seguro que tendrá un procesamiento de datos para devolver un resultado concreto
